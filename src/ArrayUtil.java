7c867039e2916838274d5bbcd7bea04c972eed7e
BohdanHorbanych <bogdan.horbanych@ringteam.com>
2024-10-31 16:51:04 +0200

    add new tests

--- src/ArrayUtil.java
+++ src/ArrayUtil.java
@@ -1,17 +1,25 @@
-/**
- * ArrayUtil exercises.
- */
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class ArrayUtil {
 
-    /**
-     * Finds the common elements between two arrays (String values).
-     *
-     * @param array1 first array
-     * @param array2 second array
-     * @return String array with common elements
-     */
     public static String[] findCommon(String[] array1, String[] array2) {
-        // TODO fill in code here
-        return new String[0];
+        Set<String> set1 = new LinkedHashSet<>();
+        Set<String> commonElements = new LinkedHashSet<>();
+
+        // Add elements from the first array to set1
+        for (String element : array1) {
+            set1.add(element);
+        }
+
+        // Check if elements in the second array exist in set1
+        for (String element : array2) {
+            if (set1.contains(element)) {
+                commonElements.add(element);
+            }
+        }
+
+        // Convert the common elements set to an array
+        return commonElements.toArray(new String[0]);
     }
 }
--- src/ArraysTest.java
+++ src/ArraysTest.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -5,11 +6,6 @@ import static org.testng.Assert.assertEquals;
 
 public class ArraysTest {
 
-    /**
-     * Test data for common array elements.
-     *
-     * @return test data array
-     */
     @DataProvider(name = "ArrayUtil")
     public static Object[][] arrays() {
         return new Object[][]{
@@ -22,15 +18,28 @@ public class ArraysTest {
                         new String[]{"here", "is", "some", "text", ".", "text"},
                         new String[]{"some", "text", "above", "."},
                         new String[]{"some", "text", "."}
+                },
+                {
+                        new String[]{"apple", "banana", "cherry"},
+                        new String[]{"banana", "apple", "kiwi"},
+                        new String[]{"apple", "banana"}
+                },
+                {
+                        new String[]{"blue", "green", "red"},
+                        new String[]{"green", "yellow", "blue"},
+                        new String[]{"green", "blue"}
                 }
-                // TODO add 2 more test data here
         };
     }
 
     @Test(dataProvider = "ArrayUtil")
     public void testFindCommon(String[] array1, String[] array2, String[] expectedResult) {
+        String[] actualResult = ArrayUtil.findCommon(array1, array2);
 
-        assertEquals(ArrayUtil.findCommon(array1, array2), expectedResult,
-                "Common elements are not correct");
+        // Sort both arrays before comparison
+        Arrays.sort(actualResult);
+        Arrays.sort(expectedResult);
+
+        assertEquals(actualResult, expectedResult, "Common elements are not correct");
     }
 }
