a14a8e442c7ab2da2a2ff0674d4df836db7abf23
BohdanHorbanych <bogdan.horbanych@ringteam.com>
2024-11-10 18:47:22 +0200

    add tests

--- src/ExampleExceptionTest.java
+++ src/ExampleExceptionTest.java
@@ -6,42 +6,38 @@ import org.testng.annotations.Test;
 
 public class ExampleExceptionTest {
 
-    /**
-     * Test data for positive test.
-     *
-     * @return test data array
-     */
     @DataProvider(name = "data")
     public static Object[][] data() {
         return new Object[][]{
                 {2, 2, 4},
-                {2, 3, 6}
-                // TODO add 2 more test data here
+                {2, 3, 6},
+                {5, 5, 25},
+                {6, 7, 42}
         };
     }
 
-    /**
-     * Test data for exception test.
-     *
-     * @return test data array
-     */
     @DataProvider(name = "negativeData")
     public static Object[][] negativeData() {
         return new Object[][]{
                 {-2, 2},
-                {2, -2}
-                // TODO add 2 more test data here
+                {2, -2},
+                {-3, 4},
+                {4, -6}
         };
     }
 
     @Test(dataProvider = "data")
     public void testRectangleArea(int a, int b, int c) {
-        // TODO put your code here
+        assertEquals(ExampleException.rectangleArea(a, b), c);
     }
-
 
     @Test(dataProvider = "negativeData")
     public void testRectangleAreaNegative(int a, int b) {
-        // TODO put your code here
+        try {
+            ExampleException.rectangleArea(a, b);
+            fail("Expected IllegalArgumentException was not thrown");
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "input value is below zero!");
+        }
     }
 }
