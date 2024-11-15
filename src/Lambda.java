6b49984938bd42c2cc38cdabf31b80fb61c2748d
BohdanHorbanych <bogdan.horbanych@ringteam.com>
2024-11-15 23:45:20 +0200

    add tests

--- src/Lambda.java
+++ src/Lambda.java
@@ -9,8 +9,8 @@ public class Lambda {
      * @return lambda expression.
      */
     public static BiFunction<Double, Double, Double> getMax() {
-        // TODO replace null with lambda expression
-        return null;
+        // Lambda expression to return the larger of two values.
+        return (a, b) -> a > b ? a : b;
     }
 
     /**
@@ -19,7 +19,7 @@ public class Lambda {
      * @return lambda expression.
      */
     public static Function<Double, Double> getSqrt() {
-        // TODO replace null with method reference
-        return null;
+        // Method reference to return the square root of a value.
+        return Math::sqrt;
     }
 }
--- src/LambdaTest.java
+++ src/LambdaTest.java
@@ -15,8 +15,10 @@ public class LambdaTest {
                 {-1.0, 10.0, 10.0},
                 {0.0, 0.0, 0.0},
                 {-10.0, NaN, NaN},
-                {POSITIVE_INFINITY, 1e32, POSITIVE_INFINITY}
-                // TODO add 2 more test data here
+                {POSITIVE_INFINITY, 1e32, POSITIVE_INFINITY},
+                // Additional test cases
+                {-5.0, -10.0, -5.0},
+                {1.0, -1.0, 1.0}
         };
     }
 
@@ -25,8 +27,10 @@ public class LambdaTest {
         return new Object[][]{
                 {25.0, 5.0},
                 {-1.0, NaN},
-                {POSITIVE_INFINITY, POSITIVE_INFINITY}
-                // TODO add 2 more test data here
+                {POSITIVE_INFINITY, POSITIVE_INFINITY},
+                // Additional test cases
+                {0.0, 0.0},
+                {49.0, 7.0}
         };
     }
 
